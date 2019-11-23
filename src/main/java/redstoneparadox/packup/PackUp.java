1ef35073242635acfb2a882132cb317fdbf301c9
RedstoneParadox <spinjitzu00@gmail.com>
2019-11-22 20:24:46 -0800

    Moved recipe template stuff into mod

--- src/main/java/redstoneparadox/packup/PackUp.java
+++ src/main/java/redstoneparadox/packup/PackUp.java
@@ -3,6 +3,7 @@ package redstoneparadox.packup;
 import net.fabricmc.api.ModInitializer;
 
 public class PackUp implements ModInitializer {
+
     @Override
     public void onInitialize() {
 
---
+++ src/main/java/redstoneparadox/packup/recipe/Template.java
@@ -0,0 +1,13 @@
+package redstoneparadox.packup.recipe;
+
+import net.minecraft.util.Pair;
+
+import java.util.List;
+
+public class Template {
+    public final List<Pair<String, String>> fills;
+
+    public Template(List<Pair<String, String>> fills) {
+        this.fills = fills;
+    }
+}
